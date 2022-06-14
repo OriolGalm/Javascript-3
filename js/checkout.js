89151f5caba5a9c251234d6b48ec93b1f118b297
OriolGalm <accions018@gmail.com>
2022-06-14 13:03:28 +0200

    Validacio forms

--- js/checkout.js
+++ js/checkout.js
@@ -3,15 +3,31 @@
 function validate() {
 	var error = 0;
 	// Get the input fields
+	
 	var fName = document.getElementById("fName");
 	var fEmail = document.getElementById("fEmail");
+	var fAddress = document.getElementById("fAddress");
+
+
+	if(fName.value.length && fAddress.value.length <= 2){
+		fName.style.border = "2px solid red";
+		fAddress.style.border = "2px solid red";
+	}else{
+		fName.style.border = "2px solid green";
+		fAddress.style.border = "2px solid green";
+	}
+	if(fEmail.value.length <= 2){
+		fEmail.style.border = "2px solid red";
+	}else{
+		fEmail.style.border = "2px solid green";
+	}
 
 	// Get the error elements
 	var errorName = document.getElementById("errorName");
 	var errorEmail = document.getElementById("errorEmail");  
 	
 	// Validate fields entered by the user: name, phone, password, and email
-	if(fName.value == ""){
+	/* if(fName.value == ""){
 		error++;
 	}
 
@@ -23,6 +39,6 @@ function validate() {
 		alert("Error");
 	}else{
 		alert("OK");
-	}
+	} */
 
 }
